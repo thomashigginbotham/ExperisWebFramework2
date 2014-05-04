83213068ead431140ee2e58a495d8a84a11646c6
Thomas Higginbotham <thomas.higginbotham@experis.com>
2014-05-04 16:19:51 -0400

    Updated Bower ignore files and dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,6 +76,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 
- 	grunt.registerTask('default', ['connect', 'watch']);
+	grunt.registerTask('default', ['connect', 'watch']);
 	grunt.registerTask('dist', ['compass:dist', 'requirejs']);
 };
