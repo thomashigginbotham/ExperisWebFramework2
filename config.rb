c17d8f67bc518f2443e169b0d14691c18fa5e23a
Thomas Higginbotham <thomas.higginbotham@experis.com>
2014-05-23 07:19:43 -0400

    Fix Compass directory structure

--- config.rb
+++ config.rb
@@ -1,9 +1,16 @@
-# Directory Settings
-http_path = "/"
+# Project Directories
 css_dir = "app/stylesheets"
 sass_dir = "app/stylesheets"
 images_dir = "app/images"
+generated_images_dir = "app/images/sprites"
 javascripts_dir = "app/javascripts"
+
+# Production Paths
+http_path = "/"
+http_images_path = "/images";
+http_generated_images_path = "/images/sprites";
+http_stylesheets_path = "/stylesheets";
+http_javascripts_path = "/javascripts";
 
 # CSS Output Style (:expanded, :nested, :compact, or :compressed)
 output_style = :expanded
