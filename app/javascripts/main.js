b573e115df79d7101902fd34503273088fbe6fdc
Thomas Higginbotham <thomas.higginbotham@experis.com>
2014-07-19 22:37:23 -0400

    Use Almond instead of RequireJS in built scripts

--- app/javascripts/main.js
+++ app/javascripts/main.js
@@ -7,8 +7,7 @@ requirejs.config({
 	baseUrl: '/javascripts/',
 	paths: {
 		samplePlugin: 'lib/jquery-plugins/jquery.sampleplugin',
-		jquery: '../bower_components/jquery/dist/jquery',
-		requirejs: '../bower_components/requirejs/require'
+		jquery: '../bower_components/jquery/dist/jquery'
 	},
 	packages: [
 
